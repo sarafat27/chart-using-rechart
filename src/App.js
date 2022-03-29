9ac2a7415cd252a31dd635ee7162832cd71e9286
sarafat27 <sarafat2769@gmail.com>
2022-03-30 00:01:09 +0600

    bar chart added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Chart from './Components/Chart/Chart';
+import SpecialChart from './Components/SpecialChart/SpecialChart';
 
 function App() {
   return (
     <div className="App">
       <Chart></Chart>
+      <SpecialChart></SpecialChart>
     </div>
   );
 }
